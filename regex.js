d88827fd2f94be750b63d31c9173174d517a3949
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-08-29 09:03:25 +0530

    Add condition to catch arbitrary value

--- regex.js
+++ regex.js
@@ -12,11 +12,11 @@ process.argv.slice(2).map((a, i) => {
     arr.push(d[0])
     return map.set('_', arr)
   }
-
   let e = /[a-z]+|[0-9]+/ig.exec(a)
-  if (/^(-){1,2}([a-z]+)$/ig.exec(process.argv[2 + i - 1]) !== null) {
+  if (/^(-){1,2}([a-z]+)$/ig.exec(process.argv[2 + i - 1]) !== null)
     return map.set(/^(-){1,2}([a-z]+)$/ig.exec(process.argv[2 + i - 1])[2], e[0])
-  }
+  arr.push(e[0])
+  return map.set('_', arr)
 })
 map.set('$0', require('path').basename(process.argv[1]))
 console.log(map)
