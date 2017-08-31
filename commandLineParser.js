77ddc46f21ad35fae44ac0ce83da359d4579a076
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-09-01 00:32:11 +0530

    Add fix to store same flag values in an array

--- commandLineParser.js
+++ commandLineParser.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 let map = new Map()
 let arr = []
+const parseFlag = (a, b) => {
+  if (map.get(a) === undefined) map.set(a, [])
+  let tempVal = map.get(a)
+  tempVal.push(b)
+  return map.set(a, tempVal)
+}
 process.argv.slice(2).map((a, i) => {
   // For catching expression flags like --a=4
   let b = /^(-){1,2}([a-z]+)(=){1}([\d]+|[a-z]+)$/ig.exec(a)
-  if (b !== null) return map.set(b[2], b[4])
-
+  if (b !== null) parseFlag(b[2], b[4])
   // For catching expression flags like --a
   let c = /^(-){1,2}([a-z]+)$/ig.exec(a)
-  if (c !== null) return map.set(c[2], undefined)
-
+  if (c !== null) {
+    if (map.get(c[2]) === undefined) {
+      return map.set(c[2], [])
+    }
+    return map.set(c[2], map.get(c[2]))
+  }
   // For catching expression arbitrary a=4
   let d = /^([a-z]+)(=){1}([\d]+|[a-z]+)$/ig.exec(a)
   if (d !== null) {
@@ -21,14 +30,10 @@ process.argv.slice(2).map((a, i) => {
   // For catching earlier value if it's in the format --flag
   let f = /^(-){1,2}([a-z]+)$/ig.exec(process.argv[2 + i - 1])
   if (f !== null) {
-    if (e[0].length === a.length) {
-      return map.set(f[2], e[0])
-    }
+    if (e[0].length === a.length) return parseFlag(f[2], e[0])
     // For catching links like https://github.com http://github.com www.google.com
     let g = /^((https:\/\/){0,1}|(https:\/\/){0,1}(w){3}[.]{1}|(http:\/\/){0,1}|(w){3}[.]{1})([a-z]+[.]{1})([a-z]{3})$/ig.exec(a)
-    if (g !== null) {
-      return map.set(f[2], g[0])
-    }
+    if (g !== null) return map.set(f[2], g[0])
   }
   // For catching arbitrary value
   if (e[0].length === a.length) {
